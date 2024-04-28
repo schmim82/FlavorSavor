c43bf59b1726ef905b657703a03500e2ff6c9f2f
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-04-28 21:42:28 +0200

    Add files via upload

---
+++ programm_daten/Zutaten_daten.py
@@ -0,0 +1,30 @@
+lachssauce_L = {"Rahm in ml": 250,
+            "Lachs in gramm": 150,
+            "Salz in gramm": 15,
+            "Pfeffer in gramm": 10}
+            
+Spaghetti_L = {"Spagetty in gramm": 250,
+            "Salz in gramm": 20}
+            
+Salatsauce_L = {"Pfeffer in gramm": 20,
+            "Salz in gramm": 30}
+
+pitataschen_L = {"PitaBrot in stücke": 6,
+            "Salz in gramm": 20,
+            "Poulet in gramm": 250}
+
+bolognese_L = {"Spaghetti in gramm" : 250,
+               "Salz in gramm": 20,
+               "Schinken in gramm" : 50,
+               "Rahm in ml" : 30,
+               "Pfeffer in gramm" : 15}
+    
+Kochbuch = {"Lachssauce": lachssauce_L,
+            "Spaghetti": Spaghetti_L,
+            "Salatsauce": Salatsauce_L,
+            "pitataschen": pitataschen_L,
+            "bolognese" : bolognese_L}
+
+Speise_L = ["Vorspeise", "Hauptspeise", "Nachspeise"]
+
+Herkunft_L = ["Japan", "Thailand", "Schweiz", "Italien"]
