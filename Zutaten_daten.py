810c015d83dedde12960cfb85a64318dab5152e2
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-04-28 22:31:35 +0200

    Add files via upload

---
+++ Zutaten_daten.py
@@ -0,0 +1,49 @@
+#Vorlage für abspeichern von Rezepten
+
+#lachssauce_L = {"Rahm in ml": 250,
+#           "Lachs": 150,
+#           "Salz": 15,
+#           "Pfeffer": 10}
+            
+#Spaghetti_L = {"Spagetty": 250,
+#            "Salz": 20}
+    
+#Kochbuch = {"Lachssauce": lachssauce_L,
+#            "Spaghetti": Spaghetti_L,
+#            }
+
+#Speise_L = ["Vorspeise", "Hauptspeise", "Nachspeise"]
+
+#Herkunft_L = ["Japan", "Thailand", "Schweiz", "Italien"]
+
+#Alle Angaben von Zutaten in gramm angeben. wenn flüssig oder sonstige Mengenangabe Bitte schreibt beispielsweise:
+    #"Anzahl Salz prisen"
+    #"Rahm in ml"
+# Wichtig ist, das wenn die angabe nicht gramm ist, ist das der Key in der dictionary aus mehreren Wörter besteht.
+
+
+Nudeln_L = {"Blattspinat": 100,
+            "Rucola": 50,
+            "Knoblauch anzahl Zehen": 2,
+            "Olivenöl in ml": 100,
+            "Parmesan": 60,
+            "Mandelkerne": 25,
+            "Abgeribene Schale von Zitrone": 1,
+            "Salz prisen": 1,
+            "Zucker": 2,
+            "Spargel": 500,
+            "Bandnudeln": 400,
+            "Radieschen": 75
+            
+}
+
+Kochbuch = {"Nudeln": Nudeln_L
+
+}
+
+#Speiseliste nicht erweitern
+Speise_L = ["Vorspeise", "Hauptspeise", "Nachspeise"]
+
+Herkunft_L = ["Japan"]
+
+
