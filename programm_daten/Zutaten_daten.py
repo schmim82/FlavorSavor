adeedcd936d403f349e069306afa760fd7c6e0e7
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-04-28 21:53:35 +0200

    Add files via upload

--- programm_daten/Zutaten_daten.py
+++ programm_daten/Zutaten_daten.py
@@ -1,30 +1,32 @@
-lachssauce_L = {"Rahm in ml": 250,
-            "Lachs in gramm": 150,
-            "Salz in gramm": 15,
-            "Pfeffer in gramm": 10}
-            
-Spaghetti_L = {"Spagetty in gramm": 250,
-            "Salz in gramm": 20}
-            
-Salatsauce_L = {"Pfeffer in gramm": 20,
-            "Salz in gramm": 30}
+#Vorlage für abspeichern von Rezepten
 
-pitataschen_L = {"PitaBrot in stücke": 6,
-            "Salz in gramm": 20,
-            "Poulet in gramm": 250}
-
-bolognese_L = {"Spaghetti in gramm" : 250,
-               "Salz in gramm": 20,
-               "Schinken in gramm" : 50,
-               "Rahm in ml" : 30,
-               "Pfeffer in gramm" : 15}
+#lachssauce_L = {"Rahm in ml": 250,
+#           "Lachs": 150,
+#           "Salz": 15,
+#           "Pfeffer": 10}
+            
+#Spaghetti_L = {"Spagetty": 250,
+#            "Salz": 20}
     
-Kochbuch = {"Lachssauce": lachssauce_L,
-            "Spaghetti": Spaghetti_L,
-            "Salatsauce": Salatsauce_L,
-            "pitataschen": pitataschen_L,
-            "bolognese" : bolognese_L}
+#Kochbuch = {"Lachssauce": lachssauce_L,
+#            "Spaghetti": Spaghetti_L,
+#            }
 
+#Speise_L = ["Vorspeise", "Hauptspeise", "Nachspeise"]
+
+#Herkunft_L = ["Japan", "Thailand", "Schweiz", "Italien"]
+
+
+Kochbuch = {
+
+}
+
+#Speiseliste nicht erweitern
 Speise_L = ["Vorspeise", "Hauptspeise", "Nachspeise"]
 
-Herkunft_L = ["Japan", "Thailand", "Schweiz", "Italien"]
+Herkunft_L = []
+
+#Alle Angaben von Zutaten in gramm angeben. wenn flüssig oder sonstige Mengenangabe Bitte schreibt beispielsweise:
+    #"Anzahl Salz prisen"
+    #"Rahm in ml"
+# Wichtig ist, das wenn die angabe nicht gramm ist, ist das der Key in der dictionary aus mehreren Wörter besteht.
