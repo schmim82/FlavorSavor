6aeb9cf567c91e02467b759a952311f2f71781e9
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-01 11:20:51 +0200

    Update Zutaten_daten.py

--- Zutaten_daten.py
+++ Zutaten_daten.py
@@ -33,11 +33,50 @@ Nudeln_L = {"Blattspinat": 100,
             "Zucker": 2,
             "Spargel": 500,
             "Bandnudeln": 400,
-            "Radieschen": 75
+            "Radieschen": 75,
+            "Zubereitung 1": "Bandnudeln 10 min kochen und anschliessend warmstellen",
+            "Zubereitung 2": "...",
+            "Zubereitung 3": "servieren"
+            #Noch weitere Zubereitungsschritte wären möglich
             
 }
 
-Kochbuch = {"Nudeln": Nudeln_L
+Kuchen_L ={"Rhabarber" : 500,
+           "Zucker in EL" : 4,
+           "Halbfettquark" : 500,
+           " Eier in Stück" : 3,
+           "Maisstärke in EL" : 2,
+           "Mehl in EL" : 2,
+           "Salz prisen": 1,
+           "Rahm in dl" : 1.5,
+           "Zucker" : 50,
+
+}
+
+Parmigiana_L={"Auberginen in Stück": 4,
+              "Olivenöl in dl": 0.5,
+              "Olivenöl in EL": 0.5,
+              "Knoblauch Anzahl Zehen": 2,
+              "Gehackte Tomaten in Anzahl Dosen": 1,
+              "Salz in TL": 0.5,
+              "Pfeffer Menge": "wenig",
+              "Mozarella": 150,
+              "Parmesan": 100,
+              "Basilikumblätter Menge": "einige",
+              "Zubereitung 1": "Backofen auf 220 Grad Heissluft vorheizen.",
+              "Zubereitung 2": "Auberginen längs in ca. 5 mm dicke Scheiben schneiden, mit Öl bestreichen, auf zwei mit Backpapier belegte Bleche verteilen.",
+              "Zubereitung 3": "Backen: ca. 20 Min. im Ofen.",
+              "Zubereitung 4":"Öl in einer Pfanne warm werden lassen. Knoblauch dazupressen, andämpfen. Tomaten beigeben, würzen, aufkochen, Sauce unter gelegentlichem Rühren bei kleiner Hitze ca. 20 Min. köcheln.",
+              "Zubereitung 5": "Mozzarella zerzupfen. 3 EL Tomatensauce und die Hälfte der Auberginen in der vorbereiteten Form verteilen. Je die Hälfte von der Tomatensauce, des Mozzarellas und des Parmesans darauf verteilen. Restliche Auberginen, Tomatensauce, restlichen Mozzarella und Parmesan darauf verteilen.",
+              "Zubereitung 6": "Fertig backen: ca. 20 Min. Herausnehmen, ca. 5 Min. ruhen lassen.",
+              "Zubereitung 7": "Basilikum darüberstreuen."
+}
+
+
+Kochbuch = {"Nudeln": Nudeln_L, 
+            "Kuchen" : Kuchen_L,
+            "Parmigiana": Parmigiana_L
+
 
 }
 
