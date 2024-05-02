ab87b8e9ed3c43cf0f4de31ce880faacdddb7107
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-02 12:57:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import re
 from daten_code import Zutaten_daten as zd
-import Funktions_ablage as fa
+from funktionen_code import Funktions_ablage as fa
 
 
 # Funktionen, welche nach Funktions_ablage.py kopiert werden
