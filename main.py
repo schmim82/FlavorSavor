3b44db558be4109ab2ba7838e9d6ab5832600e79
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-19 16:39:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from funktionen_code import Funktions_ablage as fa
 
 
 # Funktionen, welche nach Funktions_ablage.py kopiert werden
-
-def csv_reader(csv):
-    return pd.read_csv(csv, sep= ";")
+def seite_1():
+    def csv_reader(csv):
+        return pd.read_csv(csv, sep= ";")
 
 
 
@@ -19,29 +19,29 @@ def csv_reader(csv):
 #Code ab hier
 
 
-Rezepte_dataframe = csv_reader("Rezepte_dataframe.CSV")
+    Rezepte_dataframe = csv_reader("Rezepte_dataframe.CSV")
 
 
 #App Design
-st.set_page_config(page_title="FlavorSavor")
-st.sidebar.title("Flavorsavor")
-st.sidebar.subheader("Entdecke die Welt der Kulinarik")
+    st.set_page_config(page_title="FlavorSavor")
+    st.sidebar.title("Flavorsavor")
+    st.sidebar.subheader("Entdecke die Welt der Kulinarik")
 
 #Kriterien generieren
 
-herkunft_L = fa.herkunft_gen(Rezepte_dataframe)
+    herkunft_L = fa.herkunft_gen(Rezepte_dataframe)
 
 #Kriterien Angabe
-choose_course = st.sidebar.radio("Wähle die gewünschte Mahlzeit aus", zd.Speise_L)
-choose_herkunft = st.sidebar.selectbox("Wähle die Herkunft des Gerichts aus", herkunft_L)
-vegetarian = st.sidebar.checkbox("Vegetarisch")
+    choose_course = st.sidebar.radio("Wähle die gewünschte Mahlzeit aus", zd.Speise_L)
+    choose_herkunft = st.sidebar.selectbox("Wähle die Herkunft des Gerichts aus", herkunft_L)
+    vegetarian = st.sidebar.checkbox("Vegetarisch")
 
 
 #Liste nach Kriterien generieren
-rezepte_liste = fa.rezepte_L_gen(choose_course, choose_herkunft, vegetarian, Rezepte_dataframe)
+    rezepte_liste = fa.rezepte_L_gen(choose_course, choose_herkunft, vegetarian, Rezepte_dataframe)
 
-if len(rezepte_liste) == 0:
-    st.title("Leider ist kein Gericht mit diesen Kriterien Hinterlegt")
+    if len(rezepte_liste) == 0:
+        st.title("Leider ist kein Gericht mit diesen Kriterien Hinterlegt")
 
 
     #Habe noch den fehler, das der Error trotzdem angezeigt wird
@@ -50,12 +50,12 @@ if len(rezepte_liste) == 0:
 
 
 
-def rezept_markdown(rezept):
-    st.title(rezept)
-    variable = 0
-    for item in zd.Kochbuch[rezept]:
-        variable += 1
-        st.markdwon(variable, item)
+    def rezept_markdown(rezept):
+        st.title(rezept)
+        variable = 0
+        for item in zd.Kochbuch[rezept]:
+            variable += 1
+            st.markdwon(variable, item)
 
         
 
@@ -64,10 +64,10 @@ def rezept_markdown(rezept):
 
 #Rezept wählen
 
-choose_recipe = st.sidebar.selectbox("Wähle das gewünschte Gericht", rezepte_liste)
-personenanzahl = st.sidebar.slider("Wähle die Personenanzahl aus", 1, 10, 2)
+    choose_recipe = st.sidebar.selectbox("Wähle das gewünschte Gericht", rezepte_liste)
+    personenanzahl = st.sidebar.slider("Wähle die Personenanzahl aus", 1, 10, 2)
 
-fa.zutaten_ausgabe(choose_recipe, zd.Kochbuch, personenanzahl)
+    fa.zutaten_ausgabe(choose_recipe, zd.Kochbuch, personenanzahl)
 
 
-
+seite_1()
