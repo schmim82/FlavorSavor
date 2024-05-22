d18feddeedb992cb165d1ba17a0a7ae115b424f5
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-22 09:25:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from github_contents import GithubContents
 import re
 from daten_code import Zutaten_daten as zd
 from funktionen_code import Funktions_ablage as fa
-from Seiten import seite_1 as s_1
+from Seiten import Seite_1 as s_1
 import bcrypt
 
 # Set constants
