9dfb63150901ee46ced95440089761e97e230c0a
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-22 18:50:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def seite_1():
     def csv_reader(csv):
         return pd.read_csv(csv, sep= ";")
 
-    Rezepte_dataframe = csv_reader("Rezepte_dataframe")
+    Rezepte_dataframe = csv_reader("Rezepte_dataframe.csv")
 
 
 
