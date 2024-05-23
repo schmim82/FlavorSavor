afd9a894313a1d35e8db52b785cef786c40182cd
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-23 15:48:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,82 +1,19 @@
-import binascii
-import os
 import streamlit as st
 import pandas as pd
+import binascii
+import bcrypt
 from github_contents import GithubContents
-import re
+
 from daten_code import Zutaten_daten as zd
 from funktionen_code import Funktions_ablage as fa
-import bcrypt
-import streamlit_authenticator as stauth
+
+from seiten import seite_1 as s_1
+from seiten import seite_2 as s_2
 
 
-
-# Funktionen, welche nach Funktions_ablage.py kopiert werden
-def seite_1():
+st.set_page_config(page_title="FlavorSavor")
 
 
-    def csv_reader(csv):
-        return pd.read_csv(csv, sep= ";")
-
-    Rezepte_dataframe = csv_reader("Rezepte_dataframe.csv")
-
-
-
-
-#Code ab hier
-
-
-    
-    
-
-
-    #App Design
-    st.set_page_config(page_title="FlavorSavor")
-    st.sidebar.title("Flavorsavor")
-    st.sidebar.subheader("Entdecke die Welt der Kulinarik")
-
-
-
-    herkunft_L = fa.herkunft_gen(Rezepte_dataframe)
-
-
-#Kriterien Angabe
-    choose_course = st.sidebar.radio("Wähle die gewünschte Mahlzeit aus", zd.Speise_L)
-    choose_herkunft = st.sidebar.selectbox("Wähle die Herkunft des Gerichts aus", herkunft_L)
-    vegetarian = st.sidebar.checkbox("Vegetarisch")
-
-
-#Liste nach Kriterien generieren
-    rezepte_liste = fa.rezepte_L_gen(choose_course, choose_herkunft, vegetarian, Rezepte_dataframe)
-
-    if len(rezepte_liste) == 0:
-        st.title("Leider ist kein Gericht mit diesen Kriterien Hinterlegt")
-
-
-    #Habe noch den fehler, das der Error trotzdem angezeigt wird
-
-
-
-
-
-    def rezept_markdown(rezept):
-        st.title(rezept)
-        variable = 0
-        for item in zd.Kochbuch[rezept]:
-            variable += 1
-            st.markdwon(variable, item)
-
-        
-
-
-
-
-#Rezept wählen
-
-    choose_recipe = st.sidebar.selectbox("Wähle das gewünschte Gericht", rezepte_liste)
-    personenanzahl = st.sidebar.slider("Wähle die Personenanzahl aus", 1, 10, 2)
-
-    fa.zutaten_ausgabe(choose_recipe, zd.Kochbuch, personenanzahl)
 
 
 # Set constants
@@ -174,15 +111,38 @@ def main():
         elif options == "Register":
             register_page()
 
-    else:
+    else:        
+
+
+        
+
+
+
         #replace the code bellow with your own code or switch to another page
-        seite_1()
-        logout_button = st.sidebar.button("Logout")
+        seiten = ("Seite_1", "Seite_2")
+        seitenauswahl = st.sidebar.selectbox("Wähle die Seite aus", seiten)
+        if seitenauswahl == "Seite_1":
+            s_1.seite_1()
+
+        elif seitenauswahl =="Seite_2":
+            s_2.seite_2()
+
+
+
+        
+
+
+
+
+
+
+
+        logout_button = st.button("Logout")
         if logout_button:
             st.session_state['authentication'] = False
             st.rerun()
 
+
+
 if __name__ == "__main__":
     main()
-
-
