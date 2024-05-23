a950e771564c43c14bd0ba49673942e4ff6f99f4
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-23 20:42:17 +0200

    Update seite_3.py

--- seiten/seite_3.py
+++ seiten/seite_3.py
@@ -6,26 +6,24 @@ from funktionen_code import Funktions_ablage as fa
 
 
 def seite_3():
+    def csv_reader(csv):
+        return pd.read_csv(csv, sep=";")
 
-   def csv_reader(csv):
-        return pd.read_csv(csv, sep= ";")
+    Rezepte_dataframe = csv_reader("Rezepte_dataframe.csv")
 
-   Rezepte_dataframe = csv_reader("Rezepte_dataframe.csv")
+    personenanzahl = st.sidebar.slider("Wähle die Personenanzahl aus", 1, 10, 2)
 
-   personenanzahl = st.sidebar.slider("Wähle die Personenanzahl aus", 1, 10, 2)
+    random_checkbox = st.sidebar.button("Random Rezept")
 
+    # Überprüfen, ob `random_rezept` im Session State ist, sonst initialisieren
+    if 'random_rezept' not in st.session_state:
+        st.session_state.random_rezept = None
 
-   random_checkbox = st.sidebar.button("Random Rezept")
-   if random_checkbox:
-      st.session_state.random_rezept = fa.random_rez(Rezepte_dataframe)
-      fa.zutaten_ausgabe(st.session_state.random_rezept, zd.Kochbuch, personenanzahl)
+    if random_checkbox:
+        st.session_state.random_rezept = fa.random_rez(Rezepte_dataframe)
 
-        
-
-    
-
-        
-
-
+    # Nur ausgeben, wenn `random_rezept` gesetzt ist
+    if st.session_state.random_rezept:
+        fa.zutaten_ausgabe(st.session_state.random_rezept, zd.Kochbuch, personenanzahl)
 
 
