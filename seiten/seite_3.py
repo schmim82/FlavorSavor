a7abd5cbeb26ad940e53fed0387fefb2c9f8b8d4
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-23 20:34:11 +0200

    Update seite_3.py

--- seiten/seite_3.py
+++ seiten/seite_3.py
@@ -8,10 +8,8 @@ from funktionen_code import Funktions_ablage as fa
 def seite_3():
 
 
-
    random_checkbox = st.sidebar.button("Random Rezept")
-    if random_checkbox:
-      
+   if random_checkbox:
       st.session_state.random_rezept = fa.random_rez(Rezepte_dataframe)
       fa.zutaten_ausgabe(st.session_state.random_rezept, zd.Kochbuch, personenanzahl)
 
