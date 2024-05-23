939e6947b19aeeec8c9771ffd15079a288cb2b6e
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-23 20:33:26 +0200

    Update seite_3.py

--- seiten/seite_3.py
+++ seiten/seite_3.py
@@ -1,1 +1,26 @@
+import streamlit as st
+import pandas as pd
+
+from daten_code import Zutaten_daten as zd
+from funktionen_code import Funktions_ablage as fa
+
+
+def seite_3():
+
+
+
+   random_checkbox = st.sidebar.button("Random Rezept")
+    if random_checkbox:
+      
+      st.session_state.random_rezept = fa.random_rez(Rezepte_dataframe)
+      fa.zutaten_ausgabe(st.session_state.random_rezept, zd.Kochbuch, personenanzahl)
+
+        
+
+    
+
+        
+
+
+
 
