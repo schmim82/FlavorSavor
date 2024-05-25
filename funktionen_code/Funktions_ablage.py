1cc77fe7966ec179a0df8f9cdef7083c0f0f6363
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-25 14:07:00 +0200

    Update Funktions_ablage.py

--- funktionen_code/Funktions_ablage.py
+++ funktionen_code/Funktions_ablage.py
@@ -196,61 +196,43 @@ def einkaufsliste_erstellen(einkaufsliste, Kochbuch):
     leere_dic = {}
     leere_dic_2 = {}
 
-    
-
     for key in einkaufsliste:
         dictionary = Kochbuch[key]
-
         anzahl = einkaufsliste[key]
 
         for key in dictionary:
-
             words = key.split()
-        
-
             if words[0] != "Zubereitung":
-
-                
-
                 if key not in leere_dic:
-
                     if isinstance(dictionary[key], int) or isinstance(dictionary[key], float):
                         leere_dic[key] = dictionary[key] * anzahl
-
                     else:
                         leere_dic[key] = dictionary[key]
-
                 else:
-
                     if isinstance(dictionary[key], int) or isinstance(dictionary[key], float):
-                        leere_dic[key] = leere_dic[key] + (dictionary[key] * anzahl)
-
-
+                        leere_dic[key] += dictionary[key] * anzahl
 
     for key in leere_dic:
         if isinstance(leere_dic[key], int) or isinstance(leere_dic[key], float):
             leere_dic_2[key] = leere_dic[key]
-
         else:
             words_2 = key.split()
-            if words_2[0]  not in leere_dic_2:
-
+            if words_2[0] not in leere_dic_2:
                 leere_dic_2[words_2[0]] = ""
 
-
-
-                
     for key in leere_dic_2:
         words_3 = key.split()
         if leere_dic_2[key] != "":
             if len(words_3) == 1:
-                st.markdown(f"{key} -- {leere_dic_2[key]} gramm")
-
+                # Markdown-Text mit Checkbox anzeigen
+                checkbox_key = st.checkbox(f"{key} -- {leere_dic_2[key]} gramm")
             else:
-                st.markdown(f"{key} -- {leere_dic_2[key]}")
-
+                checkbox_key = st.checkbox(f"{key} -- {leere_dic_2[key]}")
         else:
-            st.markdown(f"{key}")
+            checkbox_key = st.checkbox(f"{key}")
+
+
+
 
 
 #random rezept ausgabe
