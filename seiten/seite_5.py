a801726a6500068ba6ff924bc2d95abd9b14ddac
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-28 13:20:13 +0200

    Create seite_5.py

---
+++ seiten/seite_5.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+from daten_code import Zutaten_daten as zd
+from funktionen_code import Funktions_ablage as fa
+
+
+def seite_5():
+
+  st.sidebar.title("Flavorsavor")
+  st.sidebar.subheader("Entdecke die Welt der Kulinarik")
+
+  st.title("Willkommen bei Flavorsavor")
+  st.subtitle("Entdecke die Kunst der Kulinarik")
+  
