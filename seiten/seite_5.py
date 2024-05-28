bf133f1aef91e129ab1412066d08840e7fe00e15
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-28 13:25:38 +0200

    Update seite_5.py

--- seiten/seite_5.py
+++ seiten/seite_5.py
@@ -10,14 +10,23 @@ def seite_5():
 
   st.title("Willkommen bei Flavorsavor")
   st.header("Entdecke die Kunst der Kulinarik")
-  st.subheader("Mit unserer App tauchst du ein in die vielfältige Welt der internationalen Küche. Egal ob du nach einem bestimmten Land, einer speziellen Mahlzeit, vegetarischen Rezepten suchst oder einfach nicht weißt, was du kochen sollst – Flavorsavor hat für jeden Geschmack das passende Rezept.
 
-Features:
-- Rezept Auswahl: Filtere nach Land, Mahlzeit oder vegetarischen Optionen.
-- Personalisierte Mengenangaben: Automatische Anpassung der Zutatenmenge basierend auf der Anzahl der Personen.
-- Einkaufsliste: Füge Rezepte zur Einkaufsliste hinzu und lass alle Zutaten automatisch addieren. Während dem Einkauf kannst du die Zutaten jeweils abhaken.
-- Favoriten: Speichere deine Lieblingsrezepte für schnellen Zugriff.
-- Random Rezept: Lass dich inspirieren, wenn du nicht weißt, was du kochen sollst.
+  st.subheader("Mit unserer App tauchst du ein in die vielfältige Welt der internationalen Küche. Egal ob du nach einem bestimmten Land, einer speziellen Mahlzeit, vegetarischen Rezepten suchst oder einfach nicht weißt, was du kochen sollst – Flavorsavor hat für jeden Geschmack das passende Rezept.")
+  st.subheader("\n")
+  st.subheader("Features:")
+  st.subheader("- Rezept Auswahl: Filtere nach Land, Mahlzeit oder vegetarischen Optionen.")
+  st.subheader("- Personalisierte Mengenangaben: Automatische Anpassung der Zutatenmenge basierend auf der Anzahl der Personen.")
+  st.subheader("- Einkaufsliste: Füge Rezepte zur Einkaufsliste hinzu und lass alle Zutaten automatisch addieren. Während dem Einkauf kannst du die Zutaten jeweils abhaken.")
+  st.subheader("- Favoriten: Speichere deine Lieblingsrezepte für schnellen Zugriff.")
+  st.subheader("- Random Rezept: Lass dich inspirieren, wenn du nicht weißt, was du kochen sollst.")
+  st.subheader("Mit FlavorSavor wird Kochen zu einem kulinarischen Erlebnis. Viel Spaß beim Entdecken und Genießen!")
 
-Mit FlavorSavor wird Kochen zu einem kulinarischen Erlebnis. Viel Spaß beim Entdecken und Genießen!")
+
+
+
+
+
+
+
+
   
