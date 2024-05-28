d4420b68d2deabbbac4c5d7d2c21e4532b320d40
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-28 13:22:48 +0200

    Update seite_5.py

--- seiten/seite_5.py
+++ seiten/seite_5.py
@@ -7,9 +7,17 @@ from funktionen_code import Funktions_ablage as fa
 
 def seite_5():
 
-  st.sidebar.title("Flavorsavor")
-  st.sidebar.subheader("Entdecke die Welt der Kulinarik")
 
   st.title("Willkommen bei Flavorsavor")
   st.header("Entdecke die Kunst der Kulinarik")
+  st.subheader(f"Mit unserer App tauchst du ein in die vielfältige Welt der internationalen Küche. Egal ob du nach einem bestimmten Land, einer speziellen Mahlzeit, vegetarischen Rezepten suchst oder einfach nicht weißt, was du kochen sollst – Flavorsavor hat für jeden Geschmack das passende Rezept.
+
+Features:
+- Rezept Auswahl: Filtere nach Land, Mahlzeit oder vegetarischen Optionen.
+- Personalisierte Mengenangaben: Automatische Anpassung der Zutatenmenge basierend auf der Anzahl der Personen.
+- Einkaufsliste: Füge Rezepte zur Einkaufsliste hinzu und lass alle Zutaten automatisch addieren. Während dem Einkauf kannst du die Zutaten jeweils abhaken.
+- Favoriten: Speichere deine Lieblingsrezepte für schnellen Zugriff.
+- Random Rezept: Lass dich inspirieren, wenn du nicht weißt, was du kochen sollst.
+
+Mit FlavorSavor wird Kochen zu einem kulinarischen Erlebnis. Viel Spaß beim Entdecken und Genießen!")
   
