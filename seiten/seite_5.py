c2f56de1a3d8b27863b93a5cd3f932d7092a7deb
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-28 13:23:13 +0200

    Update seite_5.py

--- seiten/seite_5.py
+++ seiten/seite_5.py
@@ -10,7 +10,7 @@ def seite_5():
 
   st.title("Willkommen bei Flavorsavor")
   st.header("Entdecke die Kunst der Kulinarik")
-  st.subheader(f"Mit unserer App tauchst du ein in die vielfältige Welt der internationalen Küche. Egal ob du nach einem bestimmten Land, einer speziellen Mahlzeit, vegetarischen Rezepten suchst oder einfach nicht weißt, was du kochen sollst – Flavorsavor hat für jeden Geschmack das passende Rezept.
+  st.subheader("Mit unserer App tauchst du ein in die vielfältige Welt der internationalen Küche. Egal ob du nach einem bestimmten Land, einer speziellen Mahlzeit, vegetarischen Rezepten suchst oder einfach nicht weißt, was du kochen sollst – Flavorsavor hat für jeden Geschmack das passende Rezept.
 
 Features:
 - Rezept Auswahl: Filtere nach Land, Mahlzeit oder vegetarischen Optionen.
