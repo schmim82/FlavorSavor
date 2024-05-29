f06d131828863a4988759f8aa717196fd1589b7d
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-29 12:43:51 +0200

    Update seite_1.py

--- seiten/seite_1.py
+++ seiten/seite_1.py
@@ -69,7 +69,7 @@ def seite_1():
         fa.rezepte_hinzufügen(user_name, choose_recipe, personenanzahl)
 
 
-    favoriten_button = st.sidebar.button(":two_hearts:")
+    favoriten_button = st.sidebar.button(":hearts:")
 
     if favoriten_button:
 
