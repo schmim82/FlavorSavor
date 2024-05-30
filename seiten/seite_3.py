cb4bf8926c46e2d1d20f1e8a085ef8f3e9c5aa7b
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-30 19:11:39 +0200

    Update seite_3.py

--- seiten/seite_3.py
+++ seiten/seite_3.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+from daten_code import Zutaten_daten as zd
+from funktionen_code import Funktions_ablage as fa
+
 def seite_3():
     def csv_reader(csv):
         return pd.read_csv(csv, sep=";")
